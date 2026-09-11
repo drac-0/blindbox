59617c5c0231c02ee2fea76567e3f49c1ed1f086
drac-0 <muhammadfaiz050607@gmail.com>
2026-09-11 20:39:16 +0800

    user profile debug

--- db/main.py
+++ db/main.py
@@ -482,6 +482,11 @@ def claim_reservation(payload: ClaimRequest, db: Session = Depends(get_db)):
     )
 
 @app.get("/users/{user_id}", response_model=UserResponse, tags=["Users"])
+def get_user_profile(user_id: int):
+    for user in fake_users:
+        if user["user_id"] == user_id:
+            return user
+    raise HTTPException(status_code=404, detail="User tidak ditemukan")
 def get_user_profile(
     user_id: int,
     db: Session = Depends(get_db),
@@ -495,6 +500,7 @@ def get_user_profile(
         )
 
     return user
+
 
 @app.get("/users/me", response_model=UserResponse, tags=["Users"])
 def get_user_profile(
